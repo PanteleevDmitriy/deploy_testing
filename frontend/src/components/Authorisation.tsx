cd7aadda41d4e84f537db05eda507001c653330f
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2024-11-19 19:32:59 +0700

    next_init

--- frontend/src/components/Authorisation.tsx
+++
@@ -1,7 +0,0 @@
-export function Authorisation() {
-  return (
-    <div>
-     AUTHORISATION COMPONENT
-    </div>
-  );
-};
--- frontend/src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const QR_ROUTE = '/test/qr'
-export const USERS_ROUTE = '/users'
