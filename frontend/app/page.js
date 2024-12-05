ff8c9329d06a1318cc8e9ad2cf72dcbece6dfaa1
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2024-12-05 23:02:09 +0700

    page js 1

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={360}
+          height={76}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -34,10 +34,10 @@ export default function Home() {
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={40}
+              height={40}
             />
-            Deploy now
+            Deploy rrrright now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs ok?
           </a>
         </div>
       </main>
@@ -60,8 +60,8 @@ export default function Home() {
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
-            height={16}
+            width={64}
+            height={64}
           />
           Learn
         </a>
@@ -75,8 +75,8 @@ export default function Home() {
             aria-hidden
             src="/window.svg"
             alt="Window icon"
-            width={16}
-            height={16}
+            width={32}
+            height={32}
           />
           Examples
         </a>
@@ -90,10 +90,10 @@ export default function Home() {
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
-            width={16}
-            height={16}
+            width={32}
+            height={32}
           />
-          Go to nextjs.org →
+          Go to nextjs.org → tuda →
         </a>
       </footer>
     </div>
