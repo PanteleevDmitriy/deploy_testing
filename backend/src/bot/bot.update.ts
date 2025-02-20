a5c1ae5e0d045529e7083ab83b44bfdd23855a9a
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-02-20 16:24:07 +0700

    bot fix4 20/02

--- backend/src/bot/bot.update.ts
+++ backend/src/bot/bot.update.ts
@@ -1,11 +1,12 @@
 import { Update, Start, Hears, InjectBot } from 'nestjs-telegraf';
 import { Telegraf, Context } from 'telegraf';
+import { BotService } from './bot.service';
 
 @Update()
 export class BotUpdate {
   constructor(
     @InjectBot() private readonly bot: Telegraf<Context>,
-    private readonly appService: BotUpdate
+    private readonly appService: BotService
   ) {}
   @Start()
   async onStart(ctx: Context) {
