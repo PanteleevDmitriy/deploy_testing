284b44f11c4380103058130c0438bde1edee4981
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-02-20 16:37:02 +0700

    bot fix5 20/02

--- backend/src/bot/bot.module.ts
+++ backend/src/bot/bot.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { BotService } from './bot.service';
-import { session } from 'telegraf';
 import LocalSession = require('telegraf-session-local');
 import { BotUpdate } from './bot.update';
 
@@ -10,7 +9,6 @@ import { BotUpdate } from './bot.update';
     TelegrafModule.forRoot({
       token: "6904873460:AAGIuR0LoLMXsknJk4jx1mAoeDCWJG2StGk",
       middlewares: [
-        session(),
         new LocalSession({ database: 'sessions_db.json' }).middleware(),
       ],
     }),
