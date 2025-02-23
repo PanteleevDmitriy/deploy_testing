7952450eb277deaecb65dfb5505e237435f863ce
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-02-23 20:35:30 +0700

    weather fix1 23/02

--- backend/src/bot/bot.module.ts
+++ backend/src/bot/bot.module.ts
@@ -5,10 +5,13 @@ import { BotUpdate } from './bot.update';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module'; 
 import LocalSession = require('telegraf-session-local');
+import { SequelizeModule } from '@nestjs/sequelize';
+import { MoneyCourse, Weather } from './bot.model';
 
 @Module({
   imports: [
     ConfigModule,
+    SequelizeModule.forFeature([MoneyCourse, Weather]),
     forwardRef(() => UsersModule),
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
