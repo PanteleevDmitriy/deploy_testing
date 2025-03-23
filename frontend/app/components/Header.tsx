06955476fd871d34f10cee709875a3efdc7f5190
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-03-23 20:03:38 +0700

    bg fix7 23/03

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-teal-100 shadow-md z-50">
+    <header className="fixed top-0 left-0 right-0 bg-teal-200 shadow-md z-50">
       <div className="container mx-auto px-4">
         <div className="flex items-center h-[100px] justify-between">
           <button
