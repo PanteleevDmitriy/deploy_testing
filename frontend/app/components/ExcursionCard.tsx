bd019f30c4097daaf40d4cfc976747d62fe54229
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-03-24 11:16:43 +0700

    bg fix1 24/03

--- frontend/app/components/ExcursionCard.tsx
+++ frontend/app/components/ExcursionCard.tsx
@@ -21,14 +21,14 @@ export default function ExcursionCard({ excursion }: ExcursionCardProps) {
   }
 
   return (
-    <div className="bg-glass shadow-lg rounded-lg overflow-hidden">
+    <div className="bg-glass shadow-lg rounded-lg overflow-hidden p-4">
       <div className="relative w-full h-[333px] sm:h-[500px]">
         <Image
           src={excursion.photoLinks[currentImageIndex] || "/placeholder.svg"}
           alt={excursion.name}
           layout="fill"
           objectFit="cover"
-          className="rounded-t-lg"
+          className="rounded-lg"
         />
         <div className="absolute inset-0 flex items-center justify-between p-4">
           <button
@@ -50,23 +50,21 @@ export default function ExcursionCard({ excursion }: ExcursionCardProps) {
           {currentImageIndex + 1} / {excursion.photoLinks.length}
         </div>
       </div>
-      <div className="p-4 bg-glass rounded-b-lg">
-        <div className="flex justify-between items-center mb-2">
-          <h3 className="text-xl font-semibold">{excursion.name}</h3>
-          {excursion.isPopular && (
-            <span className="bg-red-500 text-white text-xs px-2 py-1 rounded-full">
-              Популярное
-            </span>
-          )}
-        </div>
-        <p className="text-gray-600">{excursion.shortDescription}</p>
-        <Link
-          href={`/excursion/${excursion.id}`}
-          className="bg-teal-600 text-white px-4 py-2 rounded-full hover:bg-teal-700 w-full text-[8px] sm:text-xs lg:text-base block text-center mt-4"
-        >
-          Подробнее
-        </Link>
+      <div className="flex justify-between items-center mt-4">
+        <h3 className="text-xl font-semibold">{excursion.name}</h3>
+        {excursion.isPopular && (
+          <span className="bg-red-500 text-white text-xs px-2 py-1 rounded-full">
+            Популярное
+          </span>
+        )}
       </div>
+      <p className="text-gray-600 mt-2">{excursion.shortDescription}</p>
+      <Link
+        href={`/excursion/${excursion.id}`}
+        className="bg-teal-600 text-white px-4 py-2 rounded-full hover:bg-teal-700 w-full block text-center mt-4"
+      >
+        Подробнее
+      </Link>
     </div>
   )
 }
