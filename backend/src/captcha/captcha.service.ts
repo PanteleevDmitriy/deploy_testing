59da05011136469867176db8939caa6430a345a2
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-04-15 13:38:54 +0700

    capcha fix2 15/04

--- backend/src/captcha/captcha.service.ts
+++ backend/src/captcha/captcha.service.ts
@@ -1,43 +1,21 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import axios from 'axios';
-import qs from 'qs';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class CaptchaService {
-  private readonly secretKey = process.env.RECAPTCHA_SECRET_KEY;
+  constructor(private readonly httpService: HttpService) {}
 
-  async verifyToken(token: string): Promise<void> {
-    console.log('Received captcha token in verifyToken():', token);
-
-    const url = 'https://www.google.com/recaptcha/api/siteverify';
-
-    const postData = qs.stringify({
-      secret: this.secretKey,
-      response: token,
-    });
+  async verifyToken(token: string): Promise<boolean> {
+    const secret = process.env.RECAPTCHA_SECRET_KEY;
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
 
     try {
-      const response = await axios.post(url, postData, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)', // важно!
-        },
-      });
-
-      const data = response.data;
-      console.log('Captcha verification response:', data);
-
-      if (!data.success) {
-        throw new UnauthorizedException('Не удалось подтвердить капчу');
-      }
-
-      console.log('✅ Captcha verified successfully');
+      const response = await lastValueFrom(this.httpService.post(url));
+      return response.data.success;
     } catch (error) {
-      console.error('❌ Error verifying captcha:', error.message);
-      if (axios.isAxiosError(error) && error.response) {
-        console.error('Response data:', error.response.data);
-      }
-      throw new UnauthorizedException('Ошибка при проверке капчи');
+      console.error('Ошибка при проверке капчи:', error);
+      return false;
     }
   }
 }
