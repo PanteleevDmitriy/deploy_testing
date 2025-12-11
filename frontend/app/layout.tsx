2c38d4cc3d4e17bf831b310252406f1319f59bea
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-12-12 05:19:14 +0700

    nefix 12.12

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className="flex flex-col">
+        {/* Фиксированный фон */}
         <BgFixed />
 
         {/* Метрика */}
