e4359892b96f3a9581ab5a4ce67f7f0be5ed2f2d
dmitriipanteleev <dmitriy7panteleev@gmail.com>
2025-12-20 20:39:18 +0700

    metrika fixergfedgr

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const metrikaSrc = "/metrika/tag.js";
+  const metrikaSrc = "https://mc.yandex.ru/metrika/tag.js";
 
   return (
     <html lang="ru" className={inter.className}>
